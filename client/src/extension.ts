70bb108459335ffd89ff8e4fe9bb28ca2a75e4e7
sam lovelace <musi.musi.mute@gmail.com>
2022-07-24 13:39:40 +0100

    better server lifecycle

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,156 +4,122 @@ import * as vscode from "vscode";
 import {
   LanguageClient,
   LanguageClientOptions,
-  ServerOptions
+  ServerOptions,
+  StreamInfo,
+  TransportKind,
 } from "vscode-languageclient/node";
 import axios from "axios";
 import { chmodSync, existsSync, writeFileSync } from "fs";
 import * as mkdirp from "mkdirp";
+import * as net from "net";
+import { Server } from "http";
+import { rejects } from "assert";
+import { create } from "domain";
 
 let outputChannel: vscode.OutputChannel;
-let client: LanguageClient | null = null;
-
-// const downloadsRoot = "https://zig.pm/zls/downloads";
-
-enum InstallationName {
-  i386_linux = "i386-linux",
-  i386_windows = "i386-windows",
-  x86_64_linux = "x86_64-linux",
-  x86_64_macos = "x86_64-macos",
-  x86_64_windows = "x86_64-windows",
-}
-
-function getDefaultInstallationName(): InstallationName | null {
-  // NOTE: Not using a JS switch because they're ugly as hell and clunky :(
-
-  const plat = process.platform;
-  const arch = process.arch;
-  if (arch === "ia32") {
-    if (plat === "linux") return InstallationName.i386_linux;
-    else if (plat === "win32") return InstallationName.i386_windows;
-  } else if (arch === "x64") {
-    if (plat === "linux") return InstallationName.x86_64_linux;
-    else if (plat === "darwin") return InstallationName.x86_64_macos;
-    else if (plat === "win32") return InstallationName.x86_64_windows;
-  }
-
-  return null;
-}
-
-// async function installExecutable(context: ExtensionContext): Promise<void> {
-//   const def = getDefaultInstallationName();
-//   if (!def) {
-//     window.showInformationMessage(`Your system isn't built by our CI!\nPlease follow the instructions [here](https://github.com/zigtools/zls#from-source) to get started!`);
-//     return;
-//   }
-
-//   return window.withProgress({
-//     title: "Installing zls...",
-//     location: vscode.ProgressLocation.Notification,
-//   }, async progress => {
-//     progress.report({message: "Downloading build runner..."});
-//     const buildRunner = (await axios.get(`${downloadsRoot}/${def}/bin/build_runner.zig`)).data;
-//     progress.report({message: "Downloading zls executable..."});
-//     const exe = (await axios.get(`${downloadsRoot}/${def}/bin/zls${def.endsWith("windows") ? ".exe" : ""}`, {
-//       responseType: "arraybuffer"
-//     })).data;
-
-//     progress.report({message: "Installing..."});
-//     const installDir = vscode.Uri.joinPath(context.globalStorageUri, "zls_install");
-//     if (!existsSync(installDir.fsPath))
-//       mkdirp.sync(installDir.fsPath);
-
-//     const zlsBinPath = vscode.Uri.joinPath(installDir, `zls${def.endsWith("windows") ? ".exe" : ""}`).fsPath;
-
-//     writeFileSync(vscode.Uri.joinPath(installDir, `build_runner.zig`).fsPath, buildRunner);
-//     writeFileSync(zlsBinPath, exe, "binary");
-
-//     chmodSync(zlsBinPath, 0o755);
-
-//     let config = workspace.getConfiguration("zls");
-//     await config.update("path", zlsBinPath, true);
-
-//     startClient(context);
-//   });
-// }
+let client: LanguageClient;
+let clientWasCreated: boolean = false;
 
 export function activate(context: ExtensionContext) {
-  outputChannel = window.createOutputChannel("Zig Language Server");
-
-//   vscode.commands.registerCommand("zls.install", async () => {
-//     installExecutable(context);
-//   });
+  outputChannel = window.createOutputChannel("kule");
 
   vscode.commands.registerCommand("kule.startServer", async () => {
-    await startClient(context);
+    await startClient().catch(() => {});
   });
 
   vscode.commands.registerCommand("kule.stopServer", async () => {
-    await stopClient();
+    await stopClient().catch(() => {});
   });
-
+  
   vscode.commands.registerCommand("kule.restartServer", async () => {
-    await stopClient();
-    await startClient(context);
+    await restartClient().catch(() => {});
   });
 
-  startClient(context);
 }
 
-function startClient(context: ExtensionContext): Promise<void> {
-  const configuration = workspace.getConfiguration("kule");
-  const kulePath = configuration.get("path", "kule");
-//   const debugLog = configuration.get("debugLog", false);
-
-  let serverOptions: ServerOptions = {
-    command: kulePath,
-    args: [ "server" ]
-    // args: debugLog ? [ "--debug-log" ] : []
-  };
-
-  // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "kule" }],
-    outputChannel,
-  };  
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "kule",
-    "kule language server",
-    serverOptions,
-    clientOptions
-  );
-
-  outputChannel.appendLine(`Attempting to use kule server @ ${kulePath}`);
-  
-  
-
-  return new Promise<void>(resolve => {
-    if (client)
-      client.start().then(() => {
-          window.showInformationMessage("kule language client started!");
-          resolve();
-      }, (err) => {
-        window.showErrorMessage("Couldn't start kule language server:\n\n" + err);
-        client = null;
-      });
-    //   client.start().catch(err => {
-    //     window.showInformationMessage("We're installing zls for you! Feel free to change your `zls.path` later if you so wish!");
-    //     installExecutable(context);
-    //     client = null;
-    //   }).then(() => {
-    //     if (client) {
-    //       window.showInformationMessage("zls language client started!");
-    //       resolve();
-    //     }
-    //   });
-  });
+function config() {
+  return workspace.getConfiguration();
 }
 
-async function stopClient(): Promise<void> {
-  if (client) await client.stop();
-  window.showInformationMessage("kule language client stopped!");
+function clientIsRunning(): boolean {
+  return clientWasCreated && client.isRunning();
+}
+
+function logAndAnnounceError(err: Error, message: string) {
+  if (err) {
+    outputChannel.appendLine("[" + err.name + "] " + err.message);
+    window.showErrorMessage(message, "Show Details").then(option => {
+      if (option) {
+        outputChannel.show(true);
+      }
+    });
+  }
+  else {
+    window.showErrorMessage(message);
+  }
+}
+
+
+function startClient(): Promise<void> {
+  if (clientIsRunning()) {
+    window.showErrorMessage("Already started kule language server, stop it before starting it again");
+    return Promise.resolve();
+  }
+  else {
+    if (clientWasCreated) {
+      client.dispose();
+    }
+    client = new LanguageClient(
+      "kule",
+      "kule Language Server",
+      {
+        command: config().get("path", "kule-server"),
+      },
+      {
+        documentSelector: [{ scheme: "file", language: "kule" }],
+        outputChannel,
+      }
+    );
+    clientWasCreated = true;
+    return client.start().then(() => {
+      window.showInformationMessage("Started kule language server");
+    },
+    (err) => {
+      window.showErrorMessage("Couldn't start kule language server");
+      return Promise.reject(err);
+    });
+  }
+}
+
+function restartClient(): Promise<void> {
+  if (clientIsRunning()) {
+    return stopClient().then(() => {
+      return startClient();
+    },
+    (err) => {
+      logAndAnnounceError(err, "Couldn't restart kule language server");
+      return Promise.reject(err);
+    })
+  }
+  else {
+    window.showErrorMessage("No running kule language server to restart");
+    return Promise.reject();
+  }
+}
+
+function stopClient(): Promise<void> {
+  if (clientIsRunning()) {
+    return client.stop().then(() => {
+      window.showInformationMessage("Stopped language server");
+    }, (err) => {
+      logAndAnnounceError(err, "Couldn't stop kule language server");
+      return Promise.reject(err);
+    });
+  }
+  else {
+      window.showErrorMessage("No running kule language server to stop");
+      return Promise.resolve();
+  }
 }
 
 export function deactivate(): Thenable<void> {
